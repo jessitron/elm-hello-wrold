0a82e122b1142fe4a9a0fe9aecc887e3cd765195
Jessica Kerr <jessitron@outpace.com>
2016-04-24 20:05:11 +0000

    refactor: separate decisions from view

--- Hello.elm
+++ Hello.elm
@@ -9,10 +9,30 @@ import Mouse
 main : Signal Html
 main =
   Mouse.position
+    |> Signal.map makeDecisions
     |> Signal.map view
 
 
-view : ( Int, Int ) -> Html
+type alias ApplicationState =
+  { x : Int
+  , y : Int
+  , picture : String
+  }
+
+
+makeDecisions : ( Int, Int ) -> ApplicationState
+makeDecisions ( x, y ) =
+  let
+    picture =
+      if x < 150 then
+        "images/deeter-left.png"
+      else
+        "images/deeter-right.png"
+  in
+    { x = x, y = y, picture = picture }
+
+
+view : ApplicationState -> Html
 view m =
   Html.div
     []
@@ -21,20 +41,13 @@ view m =
     ]
 
 
-pictureView : ( Int, Int ) -> Html
-pictureView ( x, _ ) =
-  let
-    picture =
-      if x < 150 then
-        "images/deeter-left.png"
-      else
-        "images/deeter-right.png"
-  in
-    Html.img [ Html.Attributes.src picture ] []
+pictureView : ApplicationState -> Html
+pictureView { picture } =
+  Html.img [ Html.Attributes.src picture ] []
 
 
-positionView : ( Int, Int ) -> Html
-positionView ( x, y ) =
+positionView : ApplicationState -> Html
+positionView { x, y } =
   Html.div
     []
     [ Html.text ("x = " ++ (toString x) ++ ", y = " ++ (toString y))
